898698171477ed2df3a2989070ba8d6f8b4c0b97
jeffreylue <lue.jeffrey@gmail.com>
2026-03-02 19:54:18 -0500

    Enhance department analysis SQL with insights and recommendations
    Added insights and recommendations for performance evaluation over time.

--- analysis/department_analysis.sql
+++ analysis/department_analysis.sql
@@ -1,7 +1,7 @@
 -- Purpose: Evaluate total volume and SLA metrics (breach_rate_percentage, avg_resolution_time) by department
 -- Insight: IT has the highest volume and lowest average resolution time,
 -- while operations has the highest breach rate % and average resolution time.
--- Operations and Finance are tied for second highest volume and both have higher breach rate %s than IT
+-- Operations and Finance are tied for second highest volume and both have higher breach rate %s than IT.
 SELECT
     department,
     COUNT(*) AS total_requests,
@@ -26,6 +26,23 @@ SELECT
 	ROUND(
         SUM(CASE WHEN sla_breached = 'Yes' THEN 1 ELSE 0 END)::decimal 
         / COUNT(*) * 100, 2) AS breach_rate
+FROM internal_requests
+GROUP BY 1,2
+ORDER BY 2,1
+
+-- Purpose: Evaluate the key performance metrics and changes in performance over time
+-- Insight: IT has the highest volume while maintaining a consistent breach rate,
+-- Operations has stable case volume, but their breach rate and resolution time are increasing, indicating strain
+-- Recommendation: We will need to investigate case examples, workflows, and standards to see what has changed in the past 2 months.
+SELECT
+	DATE_TRUNC('month', created_at) AS month,
+	department,
+	COUNT (*) AS total_cases,
+	SUM(CASE WHEN sla_breached = 'Yes' THEN 1 ELSE 0 END) AS cases_breached,
+	ROUND(
+        SUM(CASE WHEN sla_breached = 'Yes' THEN 1 ELSE 0 END)::decimal 
+        / COUNT(*) * 100, 2) AS breach_rate,
+	ROUND(AVG(resolution_time_hours), 2) AS avg_resolution_time
 FROM internal_requests
 GROUP BY 1,2
 ORDER BY 2,1
