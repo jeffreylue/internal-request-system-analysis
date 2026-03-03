9af574fa946e18118c238f8235d495ffb1e46754
jeffreylue <lue.jeffrey@gmail.com>
2026-03-02 19:07:48 -0500

    Update dataset_creation.sql

--- data/dataset_creation.sql
+++ data/dataset_creation.sql
@@ -1,17 +1,3 @@
--- Create table
-CREATE TABLE internal_requests (
-    request_id SERIAL PRIMARY KEY,
-    created_at DATE,
-    resolved_at DATE,
-    request_source VARCHAR(20),
-    department VARCHAR(50),
-    service_type VARCHAR(50),
-    priority VARCHAR(20),
-    resolution_time_hours INT,
-    sla_hours INT,
-    sla_breached BOOLEAN
-);
-
 -- Insert data
 INSERT INTO internal_requests 
 (created_at, resolved_at, request_source, department, service_type, priority, resolution_time_hours, sla_hours, sla_breached)
